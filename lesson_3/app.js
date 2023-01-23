c2917787e8d062423e25dd437616b58c339ba371
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-01-23 22:20:02 +0200

    Comment update

--- lesson_3/app.js
+++ lesson_3/app.js
@@ -26,6 +26,16 @@ let myNotes = ["Wake up", "Go to shower", "Make a coffee", "Get back to work"];
 getNotes(myNotes);
 
 /*
+Will cause:
+
+C:\Users\nodejs-course\lesson_3>node app.js
+Note: 1 text: Wake up
+Note: 2 text: Go to shower
+Note: 3 text: Make a coffee
+Note: 4 text: Get back to work
+*/
+
+/*
 let wrongNotesType = 12;
 getNotes(wrongNotesType);
 
