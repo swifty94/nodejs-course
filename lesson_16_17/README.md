a43f424bf576f5ec11316185807ee5e4a107901a
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-01-24 19:54:17 +0200

    Working with CLI args

---
+++ lesson_16_17/README.md
@@ -0,0 +1,8 @@
+- Working with CLI arguments in our programs
+
+- TASK - make an application that will parse the CLI arguments to add/remove/read/list notes
+- currently, it is a dummy demo application that just prints some text to stdout according to the CLI arguments
+
+- Result:
+
+![](https://github.com/swifty94/nodejs-course/blob/master/lesson_16_17/result.png)
---
+++ lesson_16_17/app.js
@@ -0,0 +1,70 @@
+const getNotes = require('./notes.js');
+const yargs = require('yargs');
+const chalk = require('chalk');
+// let myNotes = ["Wake up", "Go to shower", "Make a coffee", "Get back to work"];
+// getNotes(myNotes);
+
+/*
+    Test error output in case wrong input type
+*/
+// let wrongNotesType = 12;
+// getNotes(wrongNotesType);
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note: app.js add --title="Hello" --text="World"',
+    builder: {
+        title: {
+            describe: 'Title of the note',
+            type:'string',
+            demandOption: true
+        },
+        text: {
+            describe: 'Text of the note',
+            type:'string',
+            demandOption: true
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.green.inverse("Adding new note...", "\nTitle: "+ argv.title, "\nText: "+ argv.text))
+    }
+})
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note: app.js remove --title="Hello"',
+    builder: {
+        title: {
+            describe: 'Title of the note',
+            type:'string',
+            demandOption: true
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.yellow.inverse("Removing note...", "Title: "+ argv.title));
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes: app.js list',
+    handler: function(argv){
+        console.log(chalk.blue.inverse("Listing all notes..."));
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe: 'Read a note: app.js read --title="Hello"',
+    builder: {
+        title: {
+            describe: 'Title of the note',
+            type:'string',
+            demandOption: true
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.bgGreen.inverse("Reading note...", "Title: "+ argv.title));
+    }
+})
+
+yargs.parse();
