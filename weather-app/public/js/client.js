56ecb9a10d9914d15e2829550aef262c45c10462
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-04 06:09:42 +0200

    refactoring

--- weather-app/public/js/client.js
+++ weather-app/public/js/client.js
@@ -1,2 +1,19 @@
 console.log("Client side JavaScript code is being executed")
 // TODO: implement some client side code
+
+// var x = {};
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     x = {res:"Geolocation is not supported by this browser."}
+//   }
+// }
+
+// function showPosition(position) {
+//   x = {
+//     lat:position.coords.latitude,
+//     lon:position.coords.longitude
+//   }
+//   document.getElementById('search').value = `${x.lat}.${x.lon}`;
+// }
