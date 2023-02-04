3b8d1811d1c32be83976101ff96c4c0492c80200
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-04 05:23:47 +0200

    TODO

--- weather-app/public/js/client.js
+++ weather-app/public/js/client.js
@@ -1,7 +1,2 @@
 console.log("Client side JavaScript code is being executed")
-// similar behavior as an HTTP redirect
-// window.location.replace("http://stackoverflow.com");
-
-// similar behavior as clicking on a link
-// window.location.href = "http://stackoverflow.com";
-
+// TODO: implement some client side code
