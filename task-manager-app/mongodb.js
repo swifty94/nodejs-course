29062aa6b1cadada02eed00c6d3abc6b51aacdd4
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-13 22:30:28 +0200

    More abstraction to the function

--- task-manager-app/mongodb.js
+++ task-manager-app/mongodb.js
@@ -3,32 +3,33 @@
  */
 const MongoClient = require('mongodb').MongoClient;
 const crypto = require('crypto');
-const databaseUrl = "mongodb://127.0.0.1:27017/";
-const databaseName = "secrets";
 
-const connect = (dbUrl) => MongoClient.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, client) {
+const insertSingle = (dbUrl, dbName, collectionName, dataToInsert) => MongoClient.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, client) {
   if (err){
     return console.error(`Unable to connect to the database: ${dbUrl}.\nError stack below:\n`, err)
   }
-  console.log("ConnectionToUrl:: ", dbUrl)
-  const db = client.db(databaseName)
-  let toInsert = {
-    randomString: crypto.randomBytes(10).toString('hex'),
-    randomInt: Math.floor(Math.random() * 1000000)
-  }
-  db.collection('secret-data').insertOne(toInsert, (error, result) => {
+  console.log(`dataToInsert`, JSON.stringify(dataToInsert))
+  const db = client.db(dbName)
+  db.collection(collectionName).insertOne(dataToInsert, (error, result) => {
     if (error){
-        console.log(`Fail: \n`, error)
+        console.log(`Unable to update DB ${dbName}: Collection: ${collectionName} \n`, error)
     }
-    console.log(`DocumentToInsert`, JSON.stringify(toInsert))
-    console.log(`InsertedDocument:`,result.ops);
+    console.log(`insertOneWriteOpResultObject:`,result.ops);
     client.close()
   });
 });
 
+
+const databaseUrl = "mongodb://127.0.0.1:27017/";
+const databaseName = "secrets";
+const collectionName = 'secret-data';
 // to test
 int = 0;
 while (int < 5) {
-    connect(databaseUrl)
+    const toInsert = {
+      randomString: crypto.randomBytes(10).toString('hex'),
+      randomInt: Math.floor(Math.random() * 1000000)
+    }
+    insertSingle(databaseUrl, databaseName, collectionName, toInsert)
     int++;
 }
