03206a762a93c0ad4f690bfac83dc1e7b1da57ec
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-13 23:32:51 +0200

    + insertMany

--- task-manager-app/mongodb.js
+++ task-manager-app/mongodb.js
@@ -4,6 +4,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const crypto = require('crypto');
 
+const setRandomString = () =>{
+  return crypto.randomBytes(10).toString('hex')
+};
+const setRandomInt = () => {
+  return Math.floor(Math.random() * 1000000);
+}
+
 const insertSingle = (dbUrl, dbName, collectionName, dataToInsert) => MongoClient.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, client) {
   if (err){
     return console.error(`Unable to connect to the database: ${dbUrl}.\nError stack below:\n`, err)
@@ -19,17 +26,48 @@ const insertSingle = (dbUrl, dbName, collectionName, dataToInsert) => MongoClien
   });
 });
 
+const insertMultiple = (dbUrl, dbName, collectionName, arrayToInsert) => MongoClient.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, client) {
+  if (err){
+    return console.error(`Unable to connect to the database: ${dbUrl}.\nError stack below:\n`, err)
+  }
+  console.log(`arrayToInsert`, arrayToInsert)
+  const db = client.db(dbName)
+  db.collection(collectionName).insertMany(arrayToInsert, (error, result) => {
+    if (error){
+        console.log(`Unable to update DB ${dbName}: Collection: ${collectionName} \n`, error)
+    }
+    console.log(`insertOneWriteOpResultObject:`,result.ops);
+    client.close()
+  });
+});
 
 const databaseUrl = "mongodb://127.0.0.1:27017/";
 const databaseName = "secrets";
 const collectionName = 'secret-data';
-// to test
-int = 0;
-while (int < 5) {
-    const toInsert = {
-      randomString: crypto.randomBytes(10).toString('hex'),
-      randomInt: Math.floor(Math.random() * 1000000)
+
+/*
+*  to test insertSingle
+*/
+cnt = 0;
+while (cnt < 5) {
+    let toInsert = {
+      randomString: setRandomString(),
+      randomInt: setRandomInt()
     }
     insertSingle(databaseUrl, databaseName, collectionName, toInsert)
-    int++;
+    cnt++;
 }
+/*
+* to test insertMultiple
+ */
+let cnt = 0;
+let objArray = [];
+while (cnt < 5) {
+  let toInsert = {
+          randomString: setRandomString(),
+          randomInt: setRandomInt()
+        }
+  objArray.push(toInsert);
+  cnt++;
+}
+insertMultiple(databaseUrl, databaseName, collectionName, objArray)
