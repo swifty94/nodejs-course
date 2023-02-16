924f90b3bde0a894cfbbc3a42e1970e048dfd205
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-16 19:44:04 +0200

    Add user API method

--- task-manager-app/src/index.js
+++ task-manager-app/src/index.js
@@ -12,10 +12,11 @@ app.post('/users', (req, res) => {
     console.log('new User model created:', JSON.stringify(req.body));
     user.save().then(() => {
             console.log('new User saved to DB:', JSON.stringify(user));
+            res.send({'Response': `User created: ${user.name}`})
         }).catch((error) => {
             console.log('Error while saving to DB: ',error.message);
+            res.send({'Error': `${error.message}`})
         });
-    res.send({'Response': `User created: ${user.name}`})
 });
 
 app.listen(port, () => {
