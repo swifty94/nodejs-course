d1645dec3f620e2342338234841e08fe91c6e773
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-17 01:06:33 +0200

    async/await; lesson 96

--- task-manager-app/playground/promise-chaining-2.js
+++ task-manager-app/playground/promise-chaining-2.js
@@ -1,13 +1,35 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-// task id 63eea1354d26f0b7a935648f to delete
+// promise chaining pattern
 
-Task.findByIdAndDelete('63eea1354d26f0b7a935648f').then((task) => {
-    console.log(task);
-    return Task.countDocuments({completed: false});
-}).then((count) => {
-    console.log('Count incomplete tasks', count);
-}).catch((err) => {
-    console.error(err);
+// Task.findByIdAndDelete('63eea1354d26f0b7a935648f').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({completed: false});
+// }).then((count) => {
+//     console.log('Count incomplete tasks', count);
+// }).catch((err) => {
+//     console.error(err);
+// });
+
+// async/await pattern
+
+/**
+ *
+ * @param {string} id
+ * @param {boolean} completed
+ * @return {Promise} {Promise} number|null
+ */
+const deleteTaskAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed});
+    return count;
+}
+
+// task id 63eea0e24d26f0b7a9356487 to delete
+
+deleteTaskAndCount("63eea0e24d26f0b7a9356487", false).then((count) => {
+    console.log('Incomplete tasks: ', count);
+}).catch((error) => {
+    console.log(error);
 });
--- task-manager-app/playground/promise-chaining.js
+++ task-manager-app/playground/promise-chaining.js
@@ -4,11 +4,33 @@ const User = require('../src/models/user');
 
 // 63eea57c65db02226c370c82 Bobby user
 
-User.findByIdAndUpdate('63eea57c65db02226c370c82', {age: 21}).then((user) => {
-    console.log(user);
-    return User.countDocuments({age: 42});
-}).then((count) => {
-    console.log('Count of users with age 42 is', count);
-}).catch((err) => {
-    console.error(err);
+// promise chaining pattern
+
+// User.findByIdAndUpdate('63eea57c65db02226c370c82', {age: 21}).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({age: 42});
+// }).then((count) => {
+//     console.log('Count of users with age 42 is', count);
+// }).catch((err) => {
+//     console.error(err);
+// });
+
+// async/await pattern
+
+/**
+ *
+ * @param {string} id
+ * @param {number} age
+ * @return {Promise} {Promise} number|null
+ */
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count
+};
+
+updateAgeAndCount('63eea57c65db02226c370c82', 42).then((count) => {
+    console.log('updateAgeAndCount:', count)
+}).catch(error => {
+    console.info('Error:', error);
 });
