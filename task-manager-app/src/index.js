34034c936039d1c31abcd6b56da9690207d87c3e
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-17 04:09:32 +0200

    Moved to async/await + created catch-all for non-existing routes

--- task-manager-app/src/index.js
+++ task-manager-app/src/index.js
@@ -3,80 +3,104 @@ require('./db/mongoose');
 const os = require('os');
 const Task = require('./models/task');
 const User = require('./models/user');
+const HOST = os.hostname();
+const _PORT = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
 app.use(express.json());
-
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    console.log('new User model created:', JSON.stringify(req.body));
-    user.save().then(() => {
-            console.log('Saved to DB', JSON.stringify(user));
-            res.status(201).send({'New user': user})
-        }).catch((error) => {
-            console.log('Error saving model to DB: ',error.message);
-            res.status(400).send({'Error': error.message})
-        });
+    try {
+        await user.save();
+        res.status(201).send({'UserCreated:': user});
+    } catch (error) {
+        console.log('Error:',error.message);
+        res.status(400).send({'Error:': error.message});
+    }
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users);
-    }).catch((error) => {
-        res.status(500).send();
-    });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send(users);
+    } catch (error) {
+        console.log('Error:',error.message);
+        res.status(400).send({'Error:': error.message});
+    }
 })
 
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id;
-    User.findById(_id).then((user) => {
-        if (!user){
+app.get('/users/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const user = await User.findById(_id);
+        if (!user) {
             res.status(404).send();
         }
-        console.log(`ID ${_id} matched: `, JSON.stringify(user))
         res.send(user);
-    }).catch((error) => {
+    } catch (error) {
+        console.log('Error:',error.message);
         res.status(500).send({'Error':`ID ${_id} not found`});
-    });
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    console.log('new Task model created:', JSON.stringify(req.body));
-    task.save().then(() => {
-            console.log('Saved to DB:', JSON.stringify(task));
-            res.status(201).send({'New task': task})
-        }).catch((error) => {
-            console.log('Error saving model to DB: ',error.message);
-            res.status(400).send({'Error': error.message})
-        });
+app.post('/tasks', async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save();
+        res.status(201).send({'TaskCreated': task})
+    } catch (error) {
+        console.log('Error:', error);
+        res.status(400).send({'Error': error})
+    }
 });
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((task) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const task = await Task.find({});
         res.send(task);
-    }).catch((error) => {
-        res.status(500).send({'Error': error.message});
-    });
+    } catch (error) {
+        res.status(500).send({'Error': error});
+    }
 })
 
 
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id;
-    Task.findById(_id).then((task) => {
-        if (!task){
-            res.status(404).send();
-        }
-        console.log(`ID ${_id} matched: `, JSON.stringify(task));
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const task = await Task.findById(_id);
+        if (!task) {
+            res.status(404).send({'Response':'No such task'});
+        };
         res.send(task);
-    }).catch((error) => {
-
+    } catch (error) {
+        console.log({'Error':`ID ${_id} not found`});
         res.status(500).send({'Error':`ID ${_id} not found`});
-    });
+    }
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://${os.hostname}:${port}/`);
+app.get('*', (req, res) => {
+    const uriScheme = [];
+    app._router.stack.forEach(function(r){
+        if (r.route && r.route.path && r.route.path !== '*'){
+            const method = {
+                resource: r.route.path,
+                httpMethod: r.route.stack[0].method
+            };
+            uriScheme.push(method);
+        };
+    });
+    const description = {
+        'Resources + HTTP methods available' : uriScheme,
+        'NOTES': {
+            1: 'To do GET request click on method on this page.',
+            2: 'Use cURL, Postman/SOAP UI or any other tool to perform POST requests'
+        }
+    };
+    res.send(description);
+})
+
+const URL_BASE = `http://${HOST}:${_PORT}/`;
+app.listen(_PORT, () => {
+    console.log(`Server is running on ${URL_BASE}`);
 });
