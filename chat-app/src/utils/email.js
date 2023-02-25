5895992e016115b66c7e0ec1085769ebfdf9b342
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-02-25 03:43:51 +0200

    minor refactoring

--- chat-app/src/utils/email.js
+++ chat-app/src/utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const settings = require('./settings');
 
-var transporter = nodemailer.createTransport({
+var emailClient = nodemailer.createTransport({
   service: settings.SMTP_SERVICE,
   auth: {
     user: settings.SMTP_USERNAME,
@@ -9,17 +9,17 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
+var emailOptions = {
   from: settings.SMTP_USERNAME,
   to: 'kirillrudenko1994@gmail.com',
   subject: settings.EMAIL_SUBJECT,
   text: settings.EMAIL_TEXT
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+emailClient.sendMail(emailOptions, function(error, info){
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log('SMTPTransport.SentMessageInfo: ' + info);
   }
 });
